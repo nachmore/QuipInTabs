8734534f4ce51b25e180ac8c8503056722a273d3
Oren Nachman <code@nachmore.com>
2020-02-13 12:33:29 +1100

    Load correct userconf file with useful error feedback

--- src/qit.js
+++ src/qit.js
@@ -10,14 +10,16 @@ const APP_URL = getAppUrl()
 function getAppUrl() {
 
   try {
-    const {APP_URL} = require('./qit.userconf')
+    const {APP_URL} = require('./qit.userconf.js')
 
     return APP_URL
   } catch (err) {
     var msg = `Could not load userconf!
 
 Rename qit.userconf -> qit.userconf.js and fill in the values.
-If you've done this already then please check for syntax errors`
+If you've done this already then please check for syntax errors.
+
+Error: ${err}`
 
     console.log(msg)
     alert(msg)
